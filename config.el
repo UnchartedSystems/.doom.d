d4dda8278c3ca442cf1abc4a47d2102e50c6f02b
Uncharted Systems <dan@uncharted.systems>
2023-05-09 03:43:59 -0600

    Got obsidian into Emacs, will make evil keybinds

--- config.el
+++ config.el
@@ -55,6 +55,34 @@
 (setq cider-clojure-cli-global-options "-J-XX:-OmitStackTraceInFastThrow")
 
 
+;; Obisdian.el Settings
+
+(require 'obsidian)
+(obsidian-specify-path "~/Documents/obsidian")
+;; If you want a different directory of `obsidian-capture':
+(setq obsidian-inbox-directory "Inbox")
+
+;; Define obsidian-mode bindings
+(add-hook
+ 'obsidian-mode-hook
+ (lambda ()
+   ;; Replace standard command with Obsidian.el's in obsidian vault:
+   (local-set-key (kbd "C-c C-o") 'obsidian-follow-link-at-point)
+
+   ;; Use either `obsidian-insert-wikilink' or `obsidian-insert-link':
+   (local-set-key (kbd "C-c C-l") 'obsidian-insert-wikilink)
+
+   ;; Following backlinks
+   (local-set-key (kbd "C-c C-b") 'obsidian-backlink-jump)))
+
+;; Optionally you can also bind `obsidian-jump' and `obsidian-capture'
+;; replace "YOUR_BINDING" with the key of your choice:
+;; (global-set-key (kbd "YOUR_BINDING") 'obsidian-jump)
+;; (global-set-key (kbd "YOUR_BINDING") 'obsidian-capture)
+
+;; Activate detection of Obsidian vault
+(global-obsidian-mode t)
+
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -72,9 +100,9 @@
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
 
 (setq
- doom-font (font-spec :family "Ubuntu Mono" :size 20)
- doom-unicode-font (font-spec :family "Ubuntu Mono" :size 20)
- doom-big-font (font-spec :family "Ubuntu Mono" :size 30))
+ doom-font (font-spec :family "Ubuntu Mono" :size 24)
+ doom-unicode-font (font-spec :family "Ubuntu Mono" :size 24)
+ doom-big-font (font-spec :family "Ubuntu Mono" :size 32))
 
 ;; Can be used later for conditonally setting font family + size
 ;; (cond ((eq system-type 'darwin) "on-macos")
--- custom.el
+++ custom.el
@@ -4,7 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("c865644bfc16c7a43e847828139b74d1117a6077a845d16e71da38c8413a5aaa" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default)))
+   '("c865644bfc16c7a43e847828139b74d1117a6077a845d16e71da38c8413a5aaa" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
+ '(safe-local-variable-values
+   '((cider-clojure-cli-aliases . "-J:dev")
+     (cider-preferred-build-tool . clojure-cli))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
